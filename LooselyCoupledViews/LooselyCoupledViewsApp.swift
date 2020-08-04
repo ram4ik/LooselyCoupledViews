97f1d7aa056015bcacec51a63fc5d7b7131ecc9c
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-08-04 09:42:32 +0300

    Initial Commit

---
+++ LooselyCoupledViews/LooselyCoupledViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LooselyCoupledViewsApp.swift
+//  LooselyCoupledViews
+//
+//  Created by ramil on 04.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct LooselyCoupledViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
